f4f44f4c91638c03093b070c291fe60d8feee4fa
Dale <d.walker0357@gmail.com>
2021-02-28 16:30:01 +0000

    Ansible Edits

--- jenkins/ansible.sh
+++ jenkins/ansible.sh
@@ -1,3 +1,3 @@
 # Run ansible playbook.yaml and install docker onto the swarm plus nginx and set up the swarm
 cd ansible
-ansible-playbook -i inventory playbook.yaml
+ansible-playbook playbook.yaml -i inventory.yaml
