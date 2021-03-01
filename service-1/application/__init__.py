534f3496410dd67fa51238402615938700470a77
Dale <d.walker0357@gmail.com>
2021-03-01 10:42:41 +0000

    Deploy Script Edit

--- service-1/application/__init__.py
+++ service-1/application/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@lotr_service-6/lotr'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@lotr_service-6:3306/lotr'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config['SECRET_KEY'] = "emwgfn2489ghfn240nf203nfg9gnb2f"
 
